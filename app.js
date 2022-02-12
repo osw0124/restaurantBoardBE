f454af2d077b65b268737f73d4b9302748fd5569
osw0124 <osw0124@daum.net>
2022-02-12 20:29:56 +0900

    다시 커밋

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ const path = require('path');
 const app = express();
 
 const port = 3000;
+const auth_router = require("./routes/auth");
 const boardRouter = require('./routes/board');
+
+const requestMiddlware = (req, res, next) => {
+    console.log('Request URL:', req.originalUrl, '-', new Date());
+    next();
+};
 
 connect();
 
-app.use('/api', boardRouter);
-
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(requestMiddlware);
+app.use('/api', [boardRouter, auth_router]);
+app.use(express.urlencoded({extended: true}));
+// app.use(express.static(""));
+
 
 app.use('/node_modules', express.static(path.join('/node_modules')));
+
+
+app.listen(port, () => {
+    console.log(port, "포트로 서버가 열렸어요!");
+});
