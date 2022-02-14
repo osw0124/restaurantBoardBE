7210a988b529e094d11319cebb551372c3d853ee
spirit-B <janghoon333@naver.com>
2022-02-14 09:29:59 +0900

    몽구스 수정

--- schemas/index.js
+++ schemas/index.js
@@ -4,6 +4,7 @@ const connect = () => {
     mongoose.connect("mongodb://localhost:27017/restaurant_board", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        useCreateIndex: true,
     });
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error:"));
