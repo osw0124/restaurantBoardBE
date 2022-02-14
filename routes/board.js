7bac3d55b54c77db0991cc94bf47cf5f3bfcc264
spirit-B <janghoon333@naver.com>
2022-02-14 16:52:09 +0900

    재재수정

--- routes/board.js
+++ routes/board.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Boards = require('../schemas/boardSchema');
-const authMiddleware = require('../middleware/auth-middleware');
+const authMiddleware = require('../middlewares/auth-middleware');
 const jwt = require('jsonwebtoken');
 const shortUrl = require('node-url-shortener');
 
