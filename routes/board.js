e3e530c273047aca37071ff2d06e191aa3f0b8a8
spirit-B <janghoon333@naver.com>
2022-02-14 20:33:08 +0900

    delete test

--- routes/board.js
+++ routes/board.js
@@ -41,8 +41,8 @@ router.patch('/getpost/modify/:postid', async(req, res) => {
 
 // 게시글 삭제
 router.delete('/getpost/delete/:postid', async (req, res) => {
-    const { id } = req.params;
-    console.log(id);
+    const { postid } = req.params;
+    console.log(postid);
     await Boards.deleteOne({ _id: id });
     res.json({ success: '삭제 성공'});
 });
