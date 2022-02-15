510e3edfcefa25a832d6f873bdebf12ef2540c56
spirit-B <janghoon333@naver.com>
2022-02-15 16:02:16 +0900

    일단 커밋

--- routes/comment.js
+++ routes/comment.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const Comment = require('../schemas/commentSchema');
 const router = express.Router();
+const authMiddleware = require('../middlewares/auth-middleware');
+
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
