5f0a52ccb979b0713429a0bd67d8ff1923fea710
osw0124 <osw0124@daum.net>
2022-02-15 14:46:17 +0900

    콘솔 추가

--- routes/auth.js
+++ routes/auth.js
@@ -30,9 +30,9 @@ router.post('/register/save', async (req, res) => {
 });
 
 // 아이디, 닉네임 중복확인
-router.get('/register/check', async (req, res) => {
+router.post('/register/check', async (req, res) => {
     const {user_id, user_nick} = req.body;
-    console.log(req.body);
+    console.log(user_id, user_nick);
 
     const existUsers = await users.find({
         $or: [{user_id}, {user_nick}],
