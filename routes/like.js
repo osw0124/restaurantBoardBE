c92652a5d2dfbdb4e36cd3813f1c3bd091a91704
spirit-B <janghoon333@naver.com>
2022-02-17 01:46:48 +0900

    좋아요 데이터 보내주기 추가

--- routes/like.js
+++ routes/like.js
@@ -7,20 +7,22 @@ const authMiddleware = require('../middlewares/auth-middleware');
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
+// 좋아요 데이터 보내주기
+router.get('/getlike', async(req, res) => {
+    const LikedData = await Like.find({ post_id: req.params.postid }).populate('post_id', '_id').exec();
+    
+    res.status(200).json({ response: LikedData });
+})
+
 // 게시글 좋아요
 router.post('/like/:postid', authMiddleware, async(req, res) => {
     const { user } = res.locals;
-    const LikedData = await Like.find({ post_id: req.params.postid }).populate('post_id', '_id').exec();
-    // const boardData = await Board.findById(req.params.postid).exec();
-
-    // console.log("likecount : ", boardData.like_count);
     console.log("클라이언트 like_count : ", req.body.like_count);
 
     const variable = { post_id: req.params.postid, user_nick: user.user_nick };
     console.log("data : ", variable);
 
     await Like.create(variable);
-    res.status(200).json({ response: LikedData });
 });
 
 // 게시글 좋아요 해제
